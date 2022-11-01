22490129e6271b683a3f5e50fc5ff3d75e2d580c
LeeHuynSuk <gusehf321@naver.com>
2022-11-01 22:51:13 +0900

    [Upload] Finish To Do List Feature

---
+++ 바닐라 JS 크롬앱/js/todo.js
@@ -0,0 +1,53 @@
+const toDoForm = document.getElementById("todo-form");
+
+const toDoInput = toDoForm.querySelector("input");
+// = document.querySelector("#todo-form input")
+
+const toDoList = document.getElementById("todo-list");
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+function saveToDOs() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((todo) => todo.id !== parseInt(li.id));
+  saveToDOs();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "x";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDOs();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(saveToDOs);
+  parsedToDos.forEach(paintToDo);
+}
